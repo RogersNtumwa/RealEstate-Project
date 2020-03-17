aa3149bbf0fd02a88dad42c88ea639efa5e8e68a
Rogers <ntumwar@gmail.com>
2020-03-17 11:21:27 +0400

    commiting our realEstate project to git

---
+++ listing/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from realtor.models import realtor
+from datetime import datetime
+# Create your models here.
+
+
+class listing(models.Model):
+
+    title = models.CharField(max_length=200)
+    realtor = models.ForeignKey(realtor, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=2)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    is_published = models.BooleanField(default=True)
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    photo_1 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_2 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_3 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_4 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_5 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_6 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ listing/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'listing/listings.html')
+
+
+def listing(request):
+    return render(request, 'listing/listings.html')
+
+
+def search(request):
+    return render(request, 'listing/search.html')
