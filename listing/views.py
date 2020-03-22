6cc40088c26b68d1403c47fedeaeda26e01918a5
Rogers <ntumwar@gmail.com>
2020-03-23 00:29:34 +0400

    rendering dynamic html pages

--- listing/views.py
+++ listing/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 from .models import listing
-# Create your views here.
+from django.core.paginator import (Paginator, EmptyPage, PageNotAnInteger)
+from django.shortcuts import get_object_or_404
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
-    listings = listing.objects.all()
+    listings = listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listing/listings.html', context)
 
 
 def singlelisting(request, listing_id):
-    return render(request, 'listing/listing.html')
+    get_listing = get_object_or_404(listing, pk=listing_id)
+
+    context = {
+        'get_listing': get_listing
+    }
+    return render(request, 'listing/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listing/search.html')
+    context = {
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices
+    }
+    return render(request, 'listing/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-
+from listing.models import listing
+from realtor.models import realtor
+from listing.choices import price_choices, bedroom_choices, state_choices
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    Realtors = realtor.objects.order_by('-hire_date')
+    mvp_realtor = realtor.objects.all().filter(is_mvp=True)
+    context = {
+        "Realtors": Realtors,
+        'mvp_realtor': mvp_realtor
+    }
+    return render(request, 'pages/about.html', context)
